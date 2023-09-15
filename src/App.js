f3eccd38ec3c2fe48aabe5a82be88dfd1633b0ea
Sachin Ganvir <sachinganvir51@gmail.com>
2023-09-15 16:57:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+// import "./style.css"
+
+
+const App = () => {
+
+
+    return(
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import "../style.css"
+
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+              <h1> Sachin ganvir</h1>
+                <div className="links">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Blog</a>
+                </div>
+                    
+                    
+        </nav>
+    )
+}
+
+export default Navbar;
